b4ee0d61644369505ab8a7563da09231fed1e413
calibrated-lies <failedmainmodel@pm.me>
2025-09-06 17:37:17 -0500

    Add command line options for files

--- Makefile
+++ Makefile
@@ -1,10 +1,6 @@
-TARGET = bin/dbview
+TARGET = bin/majicdb
 SRC = $(wildcard src/*.c)
 OBJ = $(patsubst src/%.c, obj/%.o, $(SRC))
-
-run: clean default
-	./$(TARGET) -f ./mynewdb.db -n 
-	./$(TARGET) -f ./mynewdb.db -a "Timmy H.,123 Sheshire Ln.,120"
 
 default: $(TARGET)
 
--- src/main.c
+++ src/main.c
@@ -8,8 +8,46 @@
 #include "parse.h"
 
 void print_usage(char *argv[]) {
+
+	printf("Usage: %s -n -f <database file>\n", argv[0]);
+	printf("\t -n  - create new database file\n");
+	printf("\t -f  - (required) path to database file\n");
+	
 }
 
 int main(int argc, char *argv[]) { 
+
+	char *filePath = NULL;
+	bool newFile = false;
+	bool list = false;
+	int retOption;
+
+	while ((retOption = getopt(argc, argv, "nf:a:l")) != -1) {
+		switch (retOption) {
+			case 'n':
+				newFile = true;
+				break;
+			case 'f':
+				filePath = optarg;
+				break;
+			case '?':
+				printf("Unknown option -%c\n", retOption);
+				break;
+			default:
+				return -1;
+
+		}
+	}
+
+	if (filePath == NULL){
+		printf("File path is a required argument\n");
+		print_usage(argv);
+
+		return 0;
+	}
+
+	printf("Newfile %d\n", newFile);
+	printf("Filepath %s\n", filePath);
 	
+	return 0;
 }
